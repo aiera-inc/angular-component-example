05ec67b3e5d9df324fcf1457fad53d0c7b0a9188
Elliot Nash <elliot@venturemedias.com>
2022-12-07 14:50:39 -0500

    Angular component example

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# FidoTest
+# Aiera Component Example
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.2.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,65 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'fido_test';
+  constructor() {
+    this.loadModule = this.loadModule.bind(this);
+    this.checkLoaded = this.checkLoaded.bind(this);
+    this.loadScript = this.loadScript.bind(this);
+
+    this.loadScript();
+  }
+
+  loadScript() {
+    const endpoint = 'https://public.aiera.com/aiera-sdk/0.0.24/embed.js';
+    const node = document.createElement('script');
+    node.src = endpoint;
+    node.type = 'text/javascript';
+    node.async = false;
+    document.getElementsByTagName('head')[0].appendChild(node);
+
+    this.checkLoaded();
+  }
+
+  checkLoaded() {
+    if (!window.Aiera) {
+      setTimeout(this.checkLoaded, 500);
+    } else {
+      this.loadModule();
+    }
+  }
+
+  loadModule() {
+    if (window.Aiera) {
+      const Aiera = window.Aiera;
+      const eventList = new Aiera.Module(
+        'https://public.aiera.com/aiera-sdk/0.0.24/modules/EventListByTicker/index.html',
+        'example_iframe'
+      );
+      eventList.load().then(() => {
+        eventList.authenticateApiKey('9b9a25ca85b41fb3c136f9814e7e8105');
+      });
+      eventList.on('authenticated', () => {
+        eventList.configure({
+          options: {
+            ticker: 'meta',
+            darkMode: false,
+            showTitleInfo: true,
+            showRecordingDetails: true,
+            showPriceReaction: true,
+            showAudioPlayer: true,
+            showSearch: true,
+          },
+        });
+      });
+    }
+  }
+}
+
+declare global {
+  interface Window {
+    Aiera: any;
+  }
 }
