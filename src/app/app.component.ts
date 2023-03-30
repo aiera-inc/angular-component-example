afd5c54e919cc35659158eb7e335cc45125de7af
Elliot Nash <elliot@venturemedias.com>
2023-03-30 15:38:59 -0400

    Update aiera-sdk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Aiera } from 'aiera-sdk';
 export class AppComponent {
   ngOnInit() {
     const eventList = new Aiera.Module(
-      'https://public.aiera.com/aiera-sdk/0.0.37/modules/EventList/index.html',
+      'https://public.aiera.com/aiera-sdk/0.0.41/modules/EventList/index.html',
       'example_iframe'
     );
     eventList.load().then(() => {
@@ -24,6 +24,8 @@ export class AppComponent {
             { name: 'transcripts', visible: true, defaultValue: true },
             { name: 'earningsOnly', visible: true, defaultValue: true },
           ],
+          showPartials: true,
+          showSentiment: true,
         },
         overrides: {
           //   style: `
